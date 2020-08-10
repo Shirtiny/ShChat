4bb25fe8ae5a0bd9b3ea5733da416469d68973a2
shirtiny <shirtiny@gmail.com>
2020-08-10 14:14:43 +0800

    移除测试的button组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Button from "./components/common/button";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Button />
     </div>
   );
 }
